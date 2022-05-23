ed4fcba111942d79472bdcbdce13607648bc459b
oxst45 <oxst45678@gmail.com>
2022-05-23 19:02:39 +0300

    a problem to be solved: when a technology is added id is not unique

--- src/6-todolist/App6.tsx
+++ src/6-todolist/App6.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {ToDo} from "./components/ToDo";
 
+
 export type TechnologyType = {
     id: number
     tech: string
@@ -11,40 +12,56 @@ export type TechnologiesArrayType = Array<TechnologyType>;
 
 
 export function App6() {
-
-        const [technologies, setTechnology] = useState([
-            {id: 1, tech: 'HTML', isDone: true},
-            {id: 2, tech: 'CSS', isDone: true},
-            {id: 3, tech: 'LESS', isDone: false},
-            {id: 4, tech: 'JS', isDone: true},
-            {id: 5, tech: 'React', isDone: false},
-            {id: 6, tech: 'TS', isDone: false},
-            {id: 7, tech: 'Redux', isDone: false}
-        ])
+const technologiesInitialState = [
+        {id: 1, tech: 'HTML', isDone: true},
+        {id: 2, tech: 'CSS', isDone: true},
+        {id: 3, tech: 'LESS', isDone: false},
+        {id: 4, tech: 'JS', isDone: true},
+        {id: 5, tech: 'React', isDone: false},
+        {id: 6, tech: 'TS', isDone: false},
+        {id: 7, tech: 'Redux', isDone: false}
+    ]
+    const [InitialState, setInitialState] = useState(technologiesInitialState)
+        const [technologies, setTechnology] = useState(technologiesInitialState)
 
         const deleteTechnology = (id: number) => {setTechnology(
             technologies.filter((t) => t.id !== id)
         )}
 
+    type ChangeISDoneType = (id: number, checkedOrNot: boolean) => void;
+    const changeISDone: ChangeISDoneType = (id: number, checkedOrNot: boolean) => {
+        setTechnology(technologies.map((t) => t.id === id ? t = {id: t.id, tech: t.tech, isDone: checkedOrNot} : t))
+    }
 
+    const [filter, setFilter] = useState('All');
+    type FilterTechnologyType = (newFilter: 'All' | 'Acquainted' | 'Studying') => void;
 
+    const changeTechnologiesList: FilterTechnologyType = (newFilter) => {
 
+        setFilter(newFilter);
+    }
 
+    let filteredTechnologiesArray = technologies;
 
-
-
-
-
-
-
+    if (filter === 'Studying') {
+        filteredTechnologiesArray = technologies.filter((tech: TechnologyType) => !tech.isDone);
+    }
+    if (filter === 'Acquainted') {
+        filteredTechnologiesArray = technologies.filter((tech: TechnologyType) => tech.isDone);
+    }
 
     return (
         <div>
             <ToDo
-                // filteredTechnologiesArray={filteredTechnologiesArray}
-                // changeTechnologiesList={changeTechnologiesList}
+                filteredTechnologiesArray={filteredTechnologiesArray}
+                technologiesInitialState={technologiesInitialState}
+                setInitialState={setInitialState}
+                changeTechnologiesList={changeTechnologiesList}
                 deleteTechnology={deleteTechnology}
                 technologies={technologies}
+                setTechnology={setTechnology}
+                setFilter={setFilter}
+                changeISDone={changeISDone}
             />
         </div>
     );
--- src/6-todolist/components/ToDo.tsx
+++ src/6-todolist/components/ToDo.tsx
@@ -6,54 +6,59 @@ type ToDoListPropsType = {
     changeTechnologiesList: (newFilter: 'All' | 'Acquainted' | 'Studying') => void;
 };
 export function ToDo(props: any) {
+    const [inputValue, setInputValue] = useState('');
 
-    const [filter, setFilter] = useState('All');
-    type FilterTechnologyType = (newFilter: 'All' | 'Acquainted' | 'Studying') => void;
 
-    const changeTechnologiesList: FilterTechnologyType = (newFilter) => {
-
-        setFilter(newFilter);
+    const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+        console.log(inputValue)
     }
 
-    let filteredTechnologiesArray = props.technologies;
+    const OnClickSubmitHandler = (inputValue: string) => {
 
-    if (filter === 'Studying') {
-        filteredTechnologiesArray = props.technologies.filter((tech: TechnologyType) => !tech.isDone);
-    }
-    if (filter === 'Acquainted') {
-        filteredTechnologiesArray = props.technologies.filter((tech: TechnologyType) => tech.isDone);
+            const newTechnology = { id: props.technologiesInitialState.length, tech: inputValue, isDone: false };
+        props.setTechnology([...props.technologies, newTechnology]);
+        props.setInitialState([...props.technologiesInitialState, newTechnology]);
     }
 
-    const technologiesList = filteredTechnologiesArray.map((tech: TechnologyType) => {
+
+    const technologiesList = props.filteredTechnologiesArray.map((tech: TechnologyType) => {
+        let checkClick = tech.isDone;
         type onClickDeleteHandlerType = (id: number) => void;
         const onClickDeleteHandler:onClickDeleteHandlerType = (id) => {
             props.deleteTechnology(id);
+        }
+        const onClickCheckedHandler = () => {
+
+            checkClick = !checkClick;
+            tech.isDone = !tech.isDone;
+            let checkedOrNot = tech.isDone;
+            props.changeISDone(tech.id, checkedOrNot);
         }
         return (
             <li key={tech.id}>
                 <button onClick={ () => {onClickDeleteHandler(tech.id)}}>Delete</button>
                 <p>{tech.tech}</p>
-                <input type="checkbox" checked={tech.isDone}/>
+                <input type="checkbox" onClick={onClickCheckedHandler} checked={checkClick}/>
             </li>
         )
     });
 
     const OnClickAllHandler  = () => {
-        changeTechnologiesList('All');
+        props.changeTechnologiesList('All');
     }
     const OnClickAcquaintedHandler = () => {
-        console.log('Acquainted')
-        changeTechnologiesList('Acquainted');
+        props.changeTechnologiesList('Acquainted');
     }
     const OnClickStudyingHandler = () => {
-        changeTechnologiesList('Studying');
+        props.changeTechnologiesList('Studying');
     }
     return (
         <div>
             <h3>TODOLIST</h3>
             <div>
-                <input type="text"/>
-                <button>Submit</button>
+                <input type="text" value={inputValue} onChange={inputHandler}/>
+                <button onClick={() => {OnClickSubmitHandler(inputValue)}}>Submit</button>
             </div>
             <div>
                 <ul>
